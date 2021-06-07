e6fa98a312611003d0281269d489dac6a74a8c76
Aaron J. Miles <aaron.miles@rmays.com>
2021-06-07 12:33:53 +0100

    Fix the exposed secret_key, added the .gitignore and README file.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Music Room - Controller App
+
+This little project it to bring Django REST framework and REACT together giving a pleasent app to listen to music via Spotify's API.
+The users can create a room which will have permissions to who can change the song or request a vote to skip while also limiting how many people are in the created room.
--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xt=#&s7c+84vft46l)fggyg%=ar=xyyb+)*56p08n9fbewx8@_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
