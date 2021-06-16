aded5cb0dd965fb13db664aa0b3d9fe362e28ca0
Aaron J. Miles <aaron.miles@rmays.com>
2021-06-16 11:15:46 +0100

    Develop the create-room logic to have a simple UI and POST to the API returning JSON to console.log.

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+
 export default class RoomJoinPage extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <p>This is the room join page</p>;
+        return (
+            <p>This is a the room join page.</p>
+        );
     }
 }
